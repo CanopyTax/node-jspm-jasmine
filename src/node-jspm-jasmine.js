028f95b4cfbca45b0c57714ab7e6c0ed8db1efa1
Marceli.no <me@marceli.no>
2016-05-20 15:00:33 -0400

    Fix for issues #12, #17, and #26 (#27)

--- src/node-jspm-jasmine.js
+++ src/node-jspm-jasmine.js
@@ -5,12 +5,9 @@ import rimraf from 'rimraf';
 import path from "path";
 import minimatch from 'minimatch';
 import Jasmine from "jasmine";
-import { Instrumenter, Report, Collector } from 'istanbul';
-import { remap, writeReport } from "remap-istanbul";
+import { Instrumenter, Report } from 'istanbul';
+import { remap } from "remap-istanbul";
 import inlineSourceMap from "inline-source-map-comment";
-
-const coveredModules = [];
-const tranpiledFileSuffix = '__node-jspm-jasmine-transpiled__.js';
 
 export function runTests(opts, errCallback = function() {}) {
 
@@ -39,18 +36,16 @@ export function runTests(opts, errCallback = function() {}) {
 
 	const jasmine = new Jasmine();
 	const SystemJS = new jspm.Loader();
-	const Instrument = new Instrumenter();
-	const systemInstantiate = SystemJS.instantiate;
 
 	const jasmineConfig = getJasmineConfig(opts.jasmineConfig)
-	const specDir = jasmineConfig.spec_dir;
+	const specDir = jasmineConfig.spec_dir || '';
 	const specFiles = jasmineConfig.spec_files;
 	delete jasmineConfig.spec_files;
 	const helpers = jasmineConfig.helpers;
 	delete jasmineConfig.helpers;
 
 	try {
-		if ( opts.coverage ) {
+		if (opts.coverage) {
 			opts.coverage.dir = opts.coverage.dir || 'coverage';
 
 			if (opts.coverage.cleanDir) {
@@ -63,21 +58,23 @@ export function runTests(opts, errCallback = function() {}) {
 			if (!Array.isArray(coverageFilesGlobs) || coverageFilesGlobs.length === 0) {
 				console.log(`Not capturing coverage because opts.coverage.files is not a valid array of globs. Also try the '--coverage-files <glob>' command line opt`);
 			}
-			opts.coverage.files.forEach( pattern => {
+			opts.coverage.files.forEach(pattern => {
 				glob.sync(path.join(process.cwd(), pattern))
 				.forEach(file => {
+
 					let isSpec = false;
 					specFiles.forEach(specFileGlob => {
-						isSpec = isSpec || minimatch(file, specFileGlob);
+						isSpec = isSpec || minimatch(file, path.join(process.cwd(), specDir, specFileGlob));
 					});
 
 					let isHelper = false;
 					helpers.forEach(helperGlob => {
-						isHelper = isHelper || minimatch(file, helperGlob);
+						isHelper = isHelper || minimatch(file, path.join(process.cwd(), specDir, helperGlob));
 					});
 
 					if (!isSpec && !isHelper) {
-						coverageFiles[file] = 1;
+						const fileKey = getFileKey(getOSFilePath(file), process.cwd());
+						coverageFiles[fileKey] = 1;
 						atLeastOneCoverageFile = true;
 					}
 				});
@@ -88,27 +85,80 @@ export function runTests(opts, errCallback = function() {}) {
 				console.log(`Be sure to provide opts.coverage.files if using the JS API, or --coverage-files <pattern> if using the CLI. Also, be sure to put the pattern in quotes to avoid the glob being expanded before jspmjasmine executes.`);
 			}
 
-			SystemJS.instantiate = (load) => {
-				let normalizedAddress = getOSFilePath(load.address)
-				if (coverageFiles[normalizedAddress]) {
-					load.address = normalizedAddress;
-					coveredModules.push(load.address);
-					if ( load.metadata.sourceMap ) {
-						delete load.metadata.sourceMap.sourcesContent;
-						load.metadata.sourceMap.file = load.address + tranpiledFileSuffix
-						load.metadata.sourceMap.sources = load.metadata.sourceMap.sources.map(
-							filename => getOSFilePath(filename)
+			// create systemjs hook to allow Istanbul to instrument transpiled sources
+			const instrument = new Instrumenter();
+			const systemInstantiate = SystemJS.instantiate;
+			const tempDirectory = path.join(__dirname, '../no-source-map/');
+			// "instantiate" is the hook that provides the transpiled source
+			SystemJS.instantiate = function(load) {
+				try {
+					// create a unique key to store the sources of modules for the browser
+					const fileKey = getFileKey(getOSFilePath(load.address), process.cwd());
+					// exclude the dependency modules (i.e. libraries) from instrumentation
+					if (coverageFiles[fileKey]) {
+						// put file's transpiled counterpart in temp folder
+						let filename;
+						let sourceMap = '';
+						// arrange sourcemaps
+						if (load.metadata.sourceMap) {
+							filename = path.join(tempDirectory, fileKey.replace(/\//g, '|'));
+							// keeping sourcesContent causes duplicate reports
+							delete load.metadata.sourceMap.sourcesContent;
+							// this is the file being "instrumented"
+							load.metadata.sourceMap.file = filename;
+							// removing "file://" from paths
+							load.metadata.sourceMap.sources = load.metadata.sourceMap.sources.map(
+								filename => getOSFilePath(filename)
+							);
+							// inlined-sourceMap to be added to file
+							sourceMap = '\n' + inlineSourceMap(load.metadata.sourceMap);
+						} else if (load.source.trim() === fs.readFileSync(getOSFilePath(load.address), 'utf8').trim()) {
+							// actual file source is the same as load.source
+							// let the original file through
+							filename = getOSFilePath(load.address);
+						} else {
+							// there is no source, but is transpiled, so we have no choice but to
+							// create a temp file that cannot be mapped back to the original
+							// I think that we should throw an error here, telling the user to figure out
+							// why it is that no sourcemap is being generated.
+							filename = path.join(tempDirectory, fileKey.replace(/\//g, '|'));
+						}
+
+						if (filename !== getOSFilePath(load.address)) {
+							// write transpiled file with to temp directory
+							fs.writeFileSync(filename, load.source + sourceMap);
+						}
+
+						// instrument file with istanbul
+						load.source = instrument.instrumentSync(
+							load.source,
+							// make the path-like file key into something that can be used as a name
+							filename
 						)
-						fs.writeFileSync(
-							load.address + tranpiledFileSuffix,
-							load.source  + '\n' + inlineSourceMap(load.metadata.sourceMap)
-						)
-						normalizedAddress += tranpiledFileSuffix;
+						// files that are not included as dependencies in the spec
+						// still need to be evaluated. So here, we're going to exclude
+						// files that are already being imported by the specs, leaving
+						// only files that were not imported present in the "coverageFiles",
+						// which will then be imported (evaluated) when the tests conclude.
+						delete coverageFiles[fileKey]
 					}
-					load.source = Instrument.instrumentSync(load.source, normalizedAddress)
+				} catch (ex) {
+					errCallback(ex)
 				}
-				return systemInstantiate.call(SystemJS, load);
+				// call the original "instantiate" hook function
+				return systemInstantiate.call(SystemJS, load)
 			}
+
+			// storing related variables in the "opts.coverage" object to avoid
+			// having to pass more arguments to the "importTestFiles" function
+			opts.coverage.coverageFiles = coverageFiles
+			opts.coverage.tempDirectory = tempDirectory
+
+			// create temp directory
+			if (fs.existsSync(tempDirectory)) {
+				rimraf.sync(tempDirectory)
+			}
+			fs.mkdirSync(tempDirectory)
 		}
 
 		jasmine.loadConfig(jasmineConfig);
@@ -141,17 +191,73 @@ export function runTests(opts, errCallback = function() {}) {
 							importTheseTestFiles();
 						}
 					})
-					.catch(errCallback);
+					.catch((ex) => {
+						if (opts.coverage && opts.coverage.tempDirectory) {
+							// remove temporary directory
+							rimraf.sync(opts.coverage.tempDirectory)
+						}
+						errCallback(ex)
+					});
 				});
 			});
 		}
 	} catch(ex) {
-		removeTempFiles();
+		if (opts.coverage && opts.coverage.tempDirectory) {
+			// remove temporary directory
+			rimraf.sync(opts.coverage.tempDirectory)
+		}
 		errCallback(new Error(`Jasmine or Jspm may not be properly configured -- '${ex.toString()}'`));
 	}
 }
 
 function importTestFiles(SystemJS, jasmine, specDir, specFiles, coverage, errCallback) {
+
+	if (coverage) {
+		// the "onComplete" hook to prevent jasmine's self-righteous exit
+		jasmine.onComplete(function (passed) {
+			// avoid misalignment with jasmine's output
+			console.log('');
+			// import the rest of the modules not already imported (evaluated)
+			// as dependencies of specs
+			Promise.all(Object.keys(coverage.coverageFiles).map(function (file) {
+				return SystemJS.import(path.join(process.cwd(), file));
+			})).then(function () {
+				if (typeof __coverage__ === 'undefined') {
+					console.log("No coverage was collected for files matching globs " + coverage.files);
+				} else {
+					try {
+						const collector = remap(__coverage__)
+						let report = Report.create(
+							(coverage.reporter || 'html'),
+							{
+								dir: coverage.dir
+							}
+						);
+						report.writeReport(collector, true);
+						report = Report.create('text', {maxCols: 70});
+						report.writeReport(collector, true);
+						report = Report.create('text-summary');
+						report.writeReport(collector, true);
+					} catch (ex) {
+						errCallback(ex);
+					}
+				}
+				// remove temporary directory
+				rimraf.sync(coverage.tempDirectory);
+				// this is the exit strategy inside Jasmine, it takes care
+				// of cross platform exit bugs
+				jasmine.exit(0, process.platform, process.version, process.exit, jasmine.exit);
+			}).catch((ex) => {
+				// remove temporary directory
+				rimraf.sync(coverage.tempDirectory);
+				errCallback(ex);
+				// this is the exit strategy inside Jasmine, it takes care
+				// of cross platform exit bugs
+				jasmine.exit(1, process.platform, process.version, process.exit, jasmine.exit);
+			});
+		});
+	}
+
 	let numSpecGlobsLeft = specFiles.length || 0;
 	if (numSpecGlobsLeft === 0) {
 		jasmine.execute();
@@ -165,65 +271,27 @@ function importTestFiles(SystemJS, jasmine, specDir, specFiles, coverage, errCal
 				.all(files.map(file => SystemJS.import(file)))
 				.then(() => {
 					if (--numSpecGlobsLeft === 0) {
-						jasmine.env.addReporter({
-						    jasmineDone: function () {
-						    	if (coverage) {
-						    		// avoid misalignment with jasmine's output
-						    		console.log('')
-									try {
-										if (typeof __coverage__ === 'undefined') {
-											console.log(`No coverage was collected for files matching globs ${coverage.files}`);
-										} else {
-											const collector = remap(__coverage__)
-											let report = Report.create(
-												(coverage.reporter || 'html'),
-												{
-													dir: coverage.dir
-												}
-											);
-											report.writeReport(collector, true);
-											report = Report.create('text', {maxCols: 70});
-											report.writeReport(collector, true);
-											report = Report.create('text-summary');
-											report.writeReport(collector, true);
-											// remove temporary "transpiled" files
-											removeTempFiles();
-										}
-									} catch (ex) {
-										errCallback(ex);
-									}
-								}
-						    }
-						});
 						jasmine.execute();
 					}
 				})
-				.catch(errCallback);
+				.catch((ex) => {
+					// remove temporary directory
+					rimraf.sync(coverage.tempDirectory);
+					errCallback(ex);
+				});
 			});
 		});
 	}
 }
 
-function removeTempFiles() {
-	coveredModules.forEach(filename => {
-		fs.unlink(
-			filename + tranpiledFileSuffix,
-			removeTempFileCallback
-		);
-	});
-}
-function removeTempFileCallback(err) {
-	if ( err ) {
-		throw new Error(
-			'Failed to remove temporary ' +
-			'file "' + filename + '". ' +
-			'Please remove it manually.'
-		);
-	}
-}
 function getOSFilePath(filename) {
 	// might need to be more robust in the future
-	return filename.replace( 'file://', '' )
+	return filename.replace('file://', '')
+}
+
+function getFileKey(filename, basePath) {
+	if(!basePath) throw new Error('Please supply a base path!')
+	return filename.replace(basePath + '/', '')
 }
 
 function getJasmineConfig(config) {
